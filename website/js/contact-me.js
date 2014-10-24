82d6c7a8a3fe3a240465e463de8be6130cdc8055
Ben Theobald <btheobald10@gmail.com>
2014-10-24 19:35:02 +1100

    contact us form validation complete

--- website/js/contact-me.js
+++ website/js/contact-me.js
@@ -1,44 +1,44 @@
 var requiredFields = [ "firstname", "lastname", "email", "message" ];
 
-function checkContactForm() 
-{
-	var theForm = document.forms[ 0 ];
-
-	for ( var i in requiredFields ) 
-	{
-		var fieldName = requiredFields[ i ];
-
-		if ( !theForm[ fieldName ].value || theForm[ fieldName ].value == "Error" ) 
-		{
-			theForm[ fieldName ].style.color = "#f66";
-			theForm[ fieldName ].value = "Error";
-
-			var emptyFields = true; 
-		} 
-	}
-
-	if ( !emptyFields ) 
-	{
-		document.getElementById("errorMessage").remove();
-		theForm.submit(); 
-	}
-	else if (document.getElementById("errorMessage") == null)
-	{
-		var errorMessage = document.createElement('div');
-		errorMessage.id = "errorMessage";
-		errorMessage.innerHTML = "<p><i>Please fill in all fields before submitting</i></p>";
-		document.body.appendChild(errorMessage);
-	}	
-
+function checkContactForm() {
+  var theForm = document.forms[ 0 ];
+  
+  for ( var i in requiredFields ) {
+    var fieldName = requiredFields[ i ];
+    
+    if ( !theForm[ fieldName ].value || theForm[ fieldName ].value == "Error" ) {
+      theForm[ fieldName ].style.color = "#f66";
+      theForm[ fieldName ].value = "Error";
+      
+      var emptyFields = true; 
+    } 
+  }
+  
+  if ( !emptyFields )
+    {
+      document.getElementById("errorMessage").remove();
+      theForm.submit();
+    }
+    else
+    {
+      if(document.getElementById("errorMessage") == null)
+      {
+        displayError();
+      }
+    }
 }
 
-function resetField( theField ) 
-{
-	if ( theField.value == "Error" ) 
-	{
-		theField.value = "";
-	}
-	theField.style.color = "#000";
+function resetField( theField ) {
+  if ( theField.value == "Error" ) {
+    theField.value = "";
+  }
+  theField.style.color = "#000";
+}
 
-	document.getElementById("errorMessage").remove();
+function displayError()
+{
+  var element = document.createElement('div');
+  element.id = "errorMessage";
+  element.innerHTML = "Please fill in all fields before submitting the form";
+  document.body.insertBefore(element, document.body.childNodes[0]);
 }
